7f2fd3ce484a37e22249272cd4ff87e6262a217b
zbfromztl <47122199+zbfromztl@users.noreply.github.com>
2023-06-26 10:13:10 -0400

    Delete gay VATSIM-Strip-Printer-development directory

--- gay VATSIM-Strip-Printer-development/src/ClearStoredCallsigns.py
+++
@@ -1,9 +0,0 @@
-import pickle
-class ClearStoredCallsigns():
-    def __init__(self, cached_callsign_path:str) -> None:
-        callsign_file = open(cached_callsign_path, "wb")
-        pickle.dump([], callsign_file)
-        callsign_file.close()
-
-if __name__ == "__main__":
-    clear = ClearStoredCallsigns()
